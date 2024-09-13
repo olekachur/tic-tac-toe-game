ce803eef6031ba81048788096dceb325e0ed5dd1
OlexandrKachur <lexandrkachur@gmail.com>
2024-09-13 20:22:19 +0300

    add log feature

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,5 +1,11 @@
-export default function Log() {
+export default function Log({ turns }) {
+    let playersTunLog = turns;
+
     return <ol id="log">
-        
-    </ol>
+        {playersTunLog.map((playerTurn) => (
+            <li key={`${playerTurn.square.row}${playerTurn.square.col}`}>
+                {`Player ${playerTurn.player} selected ${playerTurn.square.row}, ${playerTurn.square.col}`}
+            </li>
+        ))}
+    </ol >
 }
